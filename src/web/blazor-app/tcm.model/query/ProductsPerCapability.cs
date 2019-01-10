fed9804af639f2a504a79469ffe063090573bf9c
Zoran Jovanovic <zojovano@microsoft.com>
2019-01-10 23:48:56 +0100

    added model

---
+++ src/web/blazor-app/tcm.model/query/ProductsPerCapability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcm.model.query
+{
+    /// <summary>
+    /// Each capability has its lists of products and information about presence of the capability for each product
+    /// </summary>
+    public class ProductsPerCapability
+    {
+        public ProductsPerCapabilityId ProductsPerCapabilityId { get; set; }
+
+        public Capability Capability { get; set; }
+
+        public List<ProductHasCapability> Products { get; set; }
+
+    }
+
+    public class ProductsPerCapabilityId
+    {
+        public Guid Id { get; set; }
+    }
+
+}
