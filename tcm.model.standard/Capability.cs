c63306c7c89f87ec63bb7c4b7f9cf7a468f1ecf9
Zoran Jovanovic <zojovano@microsoft.com>
2019-03-07 22:16:42 +0100

    cleaned up some files, option selection implemented

--- tcm.model.standard/Capability.cs
+++ tcm.model.standard/Capability.cs
@@ -4,17 +4,9 @@ using System.Text;
 
 namespace tcm.model
 {
-    public class CapabilityId
-    {
-        public Guid Id { get; set; }
-    }
-
 
     public class Capability
     {
-        public CapabilityId Id { get; set; }
         public string Name { get; set; }
     }
-
-
 }
---
+++ tcm.model.standard/CapabilityMatrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcm.model
+{
+    public class CapabilityMatrix
+    {
+        public string Product { get; set; }
+        public string HasCapability { get; set; }
+    }
+}
