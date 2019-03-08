085b4524c38da02e7b34c75c0e85f1d9e8791e31
Zoran Jovanovic <zojovano@microsoft.com>
2019-03-08 18:54:06 +0100

    added API for list of network capabilities

--- app/web/blazor-app/tcm.web.api/Controllers/CapabilitiesController.cs
+++ app/web/blazor-app/tcm.web.api/Controllers/CapabilitiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
@@ -9,35 +10,28 @@ using System.Web.Http.Cors;
 
 namespace tcm.web.api.Controllers
 {
-
-
     [Route("api/[controller]")]
     [ApiController]
-    [EnableCors(origins: "http://localhost:1072", headers: "*", methods: "*")]
-    public class ValuesController : ControllerBase
+    public class CapabilitiesController : ControllerBase
     {
         CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=tcmappstorage;AccountKey=qPPnZg/munGpCTXhPe9ypvfhns557CUuYvA4be53NzP+wEc/zs6XASWAlHqMLpG4z9TltL4/LEajviZV6uspvQ==;EndpointSuffix=core.windows.net");
 
-        // GET api/values
+        // GET: api/Capabilities
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        [ResponseCache(Duration = 10)]
-        public ActionResult<string> Get(string id)
+        // GET: api/Capabilities/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(string id)
         {
-            
+
             CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
             CloudTable table = tableClient.GetTableReference("tcmapp");
 
-            string partitionKey = id.Split('|')[0];
-            string rowKey = id.Split('|')[1];
-
-            TableOperation operation = TableOperation.Retrieve<Model.CapabilityEntity>(partitionKey, rowKey);
+            TableOperation operation = TableOperation.Retrieve<Model.CapabilityEntity>("capability", id);
 
             TableResult result = table.ExecuteAsync(operation).Result;
             var capability = result.Result as Model.CapabilityEntity;
@@ -45,19 +39,19 @@ namespace tcm.web.api.Controllers
             return capability.products;
         }
 
-        // POST api/values
+        // POST: api/Capabilities
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Capabilities/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
