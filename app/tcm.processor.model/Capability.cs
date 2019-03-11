a702069446f6e6de4c4023f8226f34cb135ea18f
Zoran Jovanovic <zojovano@microsoft.com>
2019-03-11 22:55:47 +0100

    added file adapter and processor componet

---
+++ app/tcm.processor.model/Capability.cs
@@ -0,0 +1,9 @@
+﻿namespace tcm.processor.model
+{
+    public class Capability
+    {
+        public string Id { get; set; }
+
+        public System.Collections.Generic.IList<CapabilityAttributes> CapabilityAttributes { get; set; }
+    }
+}
---
+++ app/tcm.processor.model/CapabilityAttributes.cs
@@ -0,0 +1,7 @@
+﻿namespace tcm.processor.model
+{
+    public class CapabilityAttributes
+    {
+        public System.Collections.Generic.IList<CapabilityAttribute> CapabilityAttribute { get; set; }
+    }
+}
