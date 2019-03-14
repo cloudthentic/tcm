274eeab8653a7d790ffa17ad6b30df25f2eea02f
Zoran Jovanovic <zojovano@microsoft.com>
2019-03-14 23:22:47 +0100

    added a lot of stuff :)

---
+++ app/tcm.processor.adapter.tableStorage/CapabilityEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace tcm.processor.adapter.tableStorage
+{
+    public class CapabilityEntity : TableEntity
+    {
+        public CapabilityEntity(string capability, string capabilityAttribute)
+        {
+            this.PartitionKey = capability;
+            this.RowKey = capabilityAttribute;
+        }
+
+        public CapabilityEntity() { }
+
+        public string products { get; set; }
+
+    }
+}
