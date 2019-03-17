06d2e29255d1029cc2ea90d0cc5efaf2e0f39a50
Zoran Jovanovic <zojovano@microsoft.com>
2019-03-17 17:55:23 +0100

    updated some nugets, added app insights custom telemetry

--- app/tcm.processor.adapter.tableStorage/TableStorageWritterAdapter.cs
+++ app/tcm.processor.adapter.tableStorage/TableStorageWritterAdapter.cs
@@ -2,6 +2,9 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.DataContracts;
 
 namespace tcm.processor.adapter.tableStorage
 {
@@ -11,28 +14,71 @@ namespace tcm.processor.adapter.tableStorage
     public class TableStorageWritterAdapter
     {
         CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=tcmappstorage;AccountKey=qPPnZg/munGpCTXhPe9ypvfhns557CUuYvA4be53NzP+wEc/zs6XASWAlHqMLpG4z9TltL4/LEajviZV6uspvQ==;EndpointSuffix=core.windows.net");
+        TelemetryClient telemetry; 
+
+        public TableStorageWritterAdapter()
+        {
+            TelemetryConfiguration config = new TelemetryConfiguration("362767d6-2f0d-48cc-b789-ae478063e59f");
+            telemetry = new TelemetryClient(config);
+        }
 
         public void WriteProductAggregateListToTableStorage(IList<model.ProductAggregate> products)
         {
+            var success = false;
+            var startTime = DateTime.UtcNow;
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            try
+            { 
+                // Application Insights correlation
+                using (var telemetryOperation = telemetry.StartOperation<RequestTelemetry>("WriteProductAggregateListToTableStorage"))
+                {
 
-            // convert Productaggregates into CapabilityAggregate
-            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-            CloudTable table = tableClient.GetTableReference("tcmapp");
 
-            CapabilityEntity entity = new CapabilityEntity();
+                    // convert Productaggregates into CapabilityAggregate
+                    CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+                    CloudTable table = tableClient.GetTableReference("tcmapp");
 
-            TableBatchOperation batchOperation = new TableBatchOperation();
-            batchOperation.InsertOrReplace(entity);
+                    TableBatchOperation batchOperation = new TableBatchOperation();
+                    foreach (var item in this.ConvertProductsToTableCapabilityEntity(products))
+                    {
+                        batchOperation.InsertOrReplace(item);
+                    }
 
-            
-            IList<TableResult> result = table.ExecuteBatchAsync(batchOperation).Result;
-            //TableResult result = table.ExecuteAsync(operation).Result;
-            
+                    IList<TableResult> result = table.ExecuteBatchAsync(batchOperation).Result;
+                    success = true;
+                    telemetry.GetMetric("TableStorageWrites").TrackValue(1);
+                    telemetry.TrackEvent("TableStorageWritterAdapter.WriteProductAggregateListToTableStorage Invoked");
+                }
+            }
+            catch (Exception ex)
+            {
+                telemetry.TrackException(ex);
+            }
+            finally
+            {
+                timer.Stop();
+                telemetry.TrackDependency("Azure Table Storage", "WriteProductAggregateListToTableStorage", startTime, timer.Elapsed, success);
+                telemetry.Flush();
+            }
+
         }
 
-        private IList<CapabilityEntity> ConvertProductsToTableCapabilityEntity(IList<model.ProductAggregate> products)
+        public IList<CapabilityEntity> ConvertProductsToTableCapabilityEntity(IList<model.ProductAggregate> products)
         {
+            model.Services.ProductToCapabilityAggregateService service = new model.Services.ProductToCapabilityAggregateService();
+            var capabilityAggregateList = service.ConvertProductToCapabilityAggregate(products);
 
+            var capabilityEntityList = new List<CapabilityEntity>();
+
+            foreach(var item in capabilityAggregateList)
+            {
+                var newEntity = new CapabilityEntity(item.CapabilityId, item.Attribute);
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(item.ListOfProductHasCapabilityAttribute);
+                newEntity.products = output;
+                capabilityEntityList.Add(newEntity);
+            }
+
+            return capabilityEntityList;
         }
     }
 }
---
+++ app/tcm.processor.app.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tcm.processor.app.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
