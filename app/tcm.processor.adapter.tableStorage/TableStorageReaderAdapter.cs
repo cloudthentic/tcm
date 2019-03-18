e0ec58b0bbfdbb2f122c5695ddb9665d2d540645
Zoran Jovanovic <zojovano@microsoft.com>
2019-03-18 10:33:59 +0100

    removing connection keys

---
+++ app/tcm.processor.adapter.tableStorage/TableStorageReaderAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace tcm.processor.adapter.tableStorage
+{
+    public class TableStorageReaderAdapter
+    {
+        CloudStorageAccount storageAccount;
+        TelemetryClient telemetry;
+
+        public TableStorageReaderAdapter(string connection)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connection);
+            TelemetryConfiguration config = new TelemetryConfiguration("362767d6-2f0d-48cc-b789-ae478063e59f");
+            telemetry = new TelemetryClient(config);
+        }
+
+        public string ReadProduct(string capability, string attribute)
+        {
+            try
+            {
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+                CloudTable table = tableClient.GetTableReference("tcmapp");
+
+                string partitionKey = capability;
+                string rowKey = attribute;
+
+                TableOperation operation = TableOperation.Retrieve<CapabilityEntity>(partitionKey, rowKey);
+
+                TableResult result = table.ExecuteAsync(operation).Result;
+                telemetry.GetMetric("TableStorageReads").TrackValue(1);
+                telemetry.TrackEvent("TableStorageReaderAdapter.ReadProduct Invoked");
+
+                var capabilityEntity = result.Result as CapabilityEntity;
+                return capabilityEntity.products;
+            }
+            catch(Exception ex)
+            {
+                telemetry.TrackException(ex);
+                throw ex;
+            }
+            
+        }
+    }
+}
---
+++ app/tcm.processor.app/processor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace tcm.processor.app
+{
+    public class Processor
+    {
+        TelemetryClient telemetry;
+
+        public Processor()
+        {
+            TelemetryConfiguration config = new TelemetryConfiguration("362767d6-2f0d-48cc-b789-ae478063e59f");
+            telemetry = new TelemetryClient(config);
+        }
+
+        public void ProcessAll(string path)
+        {
+            try
+            {
+                using (var telemetryOperation = telemetry.StartOperation<RequestTelemetry>("Processor.ProcessAll"))
+                {
+
+                    var fsa = new adapter.fileSystem.FileSystemReaderAdapter();
+
+                    var productAggregateList = fsa.ParseFileSystemWithProductHiearchy(path);
+                    var tsa = new adapter.tableStorage.TableStorageWritterAdapter("connection");
+                    tsa.WriteProductAggregateListToTableStorage(productAggregateList);
+                    telemetry.TrackEvent("Processor.ProcessAll Completed");
+                }
+            }
+            catch (Exception ex)
+            {
+                telemetry.TrackException(ex);
+                throw ex;
+            }
+            finally
+            {
+                telemetry.Flush();
+            }
+        }
+    }
+}
